12a2b24c64adde086e560a1d94d49ee1e55ab14a
Josh Miles <joshmilesproduction@gmail.com>
2015-09-08 00:00:14 +0100

    Added the TLNs/SNs projects

---
+++ src/TheSilentNet/snetTLN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSilentNet;
+
+namespace snetTLN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
