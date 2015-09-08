c4fb05d56977734aceb52b71e023832be8db008c
Splitty <splittydev@gmail.com>
2015-09-08 03:20:24 +0200

    Added a shitload of comments

--- src/TheSilentNet/TheSilentNet/Database.cs
+++ src/TheSilentNet/TheSilentNet/Database.cs
@@ -6,44 +6,119 @@ using System.Linq;
 
 namespace TheSilentNet
 {
+	/// <summary>
+	/// Database connector.
+	/// </summary>
 	public class Database : Singleton<Database>, IDisposable
 	{
+		/// <summary>
+		/// The SQLite data source.
+		/// </summary>
 		const string DATA_SOURCE = "cipc.db";
+
+		/// <summary>
+		/// The initial table layout.
+		/// </summary>
 		const string TABLE_LAYOUT =
+			
+			// Enable foreign key support.
 			"PRAGMA foreign_keys = ON;" +
+			// Create the table 'cips' only if it doesn't yet exist.
 			"CREATE TABLE IF NOT EXISTS cips (" +
+			// The cIP must not be null and is unique.
+			// The query must fail on collision.
 			"cip VARCHAR(512) NOT NULL UNIQUE ON CONFLICT FAIL," +
+			// The TLN blob indicates whether a node is a TLN.
 			"tln BLOB(1) DEFAULT 0" +
-			"); CREATE TABLE IF NOT EXISTS domains (" +
+			");" +
+			// Create the table 'domains' only if it doesn't yet exist.
+			"CREATE TABLE IF NOT EXISTS domains (" +
+			// This column is a reference to the column of same name
+			// in the 'cips' table (defined above).
 			"cip VARCHAR(512) NOT NULL UNIQUE ON CONFLICT FAIL," +
+			// The name of the domain must not be null and is unique.
+			// The query must fail on collision.
 			"name VARCHAR(64) NOT NULL UNIQUE ON CONFLICT FAIL," +
+			// The port of the host defaults to 80.
 			"port INT DEFAULT 80," +
+			// Make the 'cip' field reference the 'cip' field
+			// in the 'cips' table (defined above).
 			"FOREIGN KEY(cip) REFERENCES cips(cip)" +
 			");";
 
-		readonly string QUERY_SELECT_TLN = Instance ().Select ("cip").From ("cips").Where ("tln").Eq (1);
-		readonly string QUERY_SELECT_KNOWN_SN = Instance ().Select ("cip").From ("cips").Where ("tln").Eq (0);
-		readonly string QUERY_INSERT_TLN = Instance ().Into ("cip").Insert ("cip", "tln").Values ("@cip", 1);
-		readonly string QUERY_INSERT_SN = Instance ().Into ("cip").Insert ("cip").Values ("@cip");
+		#region Premade queries
 
+		/// <summary>
+		/// Selects the cIPs of all TLNs from the 'cips' table.
+		/// </summary>
+		readonly string QUERY_SELECT_TLN = Instance ().Select ("cip").From ("cips").Where ("tln").Eq (1);
+
+		/// <summary>
+		/// Selects the cIPs of all nodes (excluding TLNs) from the 'cips' table.
+		/// </summary>
+		readonly string QUERY_SELECT_KNOWN_SN = Instance ().Select ("cip").From ("cips").Where ("tln").Eq (0);
+
+		/// <summary>
+		/// Inserts the cIP of a TLN into the 'cips' table.
+		/// </summary>
+		readonly string QUERY_INSERT_TLN = Instance ().Into ("cips").Insert ("cip", "tln").Values ("@cip", 1);
+
+		/// <summary>
+		/// Inserts the cIP of a non-TLN into the 'cips' table.
+		/// </summary>
+		readonly string QUERY_INSERT_SN = Instance ().Into ("cips").Insert ("cip").Values ("@cip");
+
+		#endregion
+
+		/// <summary>
+		/// The SQLite connection.
+		/// </summary>
 		readonly SQLiteConnection con;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TheSilentNet.Database"/> class.
+		/// </summary>
 		public Database () {
+
+			// Make sure that nobody directly instantiates this class.
+			// It must be instantiated from within the Instance () method.
 			Guard ();
+
+			// Create and open the connection.
 			con = new SQLiteConnection {
 				ConnectionString = string.Format ("Data Source={0}", DATA_SOURCE)
 			}.OpenAndReturn ();
+
+			// Create the initial table layout.
+			// This does nothing if the tables already exist.
 			ExecNonQuery (TABLE_LAYOUT);
 		}
 
+		/// <summary>
+		/// Adds a node to the 'cips' table.
+		/// </summary>
+		/// <returns><c>true</c>, if the node was added, <c>false</c> otherwise.</returns>
+		/// <param name="cip">cIP.</param>
+		/// <param name="tln">Whether this node is a TLN.</param>
 		public bool AddNode (string cip, bool tln = false) {
 			return ExecNonQuery (tln ? QUERY_INSERT_TLN : QUERY_INSERT_SN, cip.ToSQLiteParam ("@cip")) > 0;
 		}
 
+		/// <summary>
+		/// Adds a TLN to the 'cips' table.
+		/// </summary>
+		/// <returns><c>true</c>, if the TLN was added, <c>false</c> otherwise.</returns>
+		/// <param name="cip">cIP.</param>
 		public bool AddTopLevelNode (string cip) {
 			return AddNode (cip: cip, tln: true);
 		}
 
+		/// <summary>
+		/// Gets a maximum of <paramref name="max"/> nodes from the 'cips' table.
+		/// </summary>
+		/// <returns>The nodes.</returns>
+		/// <param name="max">Node limit.</param>
+		/// <param name="tln">Whether only TLNs should be returned.</param>
 		public IEnumerable<CipEntry> GetNodes (int max = 1024, bool tln = false) {
 			var nodes = new List<CipEntry> ();
 			using (var reader = ExecReader ((tln ? QUERY_SELECT_TLN : QUERY_SELECT_KNOWN_SN).Limit (max))) {
@@ -54,10 +129,21 @@ namespace TheSilentNet
 			return nodes;
 		}
 
+		/// <summary>
+		/// Gets a maximum of <paramref name="max"/> TLNs from the 'cips' table.
+		/// </summary>
+		/// <returns>The TLNs.</returns>
+		/// <param name="max">Node limit.</param>
 		public IEnumerable<CipEntry> GetTopLevelNodes (int max = 1024) {
 			return GetNodes (max: 1024, tln: true);
 		}
 
+		/// <summary>
+		/// Creates an SQLite command.
+		/// </summary>
+		/// <returns>The command.</returns>
+		/// <param name="query">Query.</param>
+		/// <param name="args">Arguments.</param>
 		SQLiteCommand CreateCommand (string query, params KeyValuePair<string, object>[] args) {
 			var com = con.CreateCommand ();
 			com.CommandText = query;
@@ -67,20 +153,45 @@ namespace TheSilentNet
 			return com;
 		}
 
+		/// <summary>
+		/// Executes a query.
+		/// </summary>
+		/// <returns>The number of rows inserted/updated affected by the query.</returns>
+		/// <param name="query">Query.</param>
+		/// <param name="args">Arguments.</param>
 		int ExecNonQuery (string query, params KeyValuePair<string, object>[] args) {
 			return CreateCommand (query, args).ExecuteNonQuery ();
 		}
 
+		/// <summary>
+		/// Executes a query.
+		/// </summary>
+		/// <returns>The first column of the resultset (if present), null otherwise.</returns>
+		/// <param name="query">Query.</param>
+		/// <param name="args">Arguments.</param>
 		object ExecScalar (string query, params KeyValuePair<string, object>[] args) {
 			return CreateCommand (query, args).ExecuteScalar ();
 		}
 
+		/// <summary>
+		/// Executes a query.
+		/// </summary>
+		/// <returns>An SQLiteDataReader that can be used to access the resultset.</returns>
+		/// <param name="query">Query.</param>
+		/// <param name="args">Arguments.</param>
 		SQLiteDataReader ExecReader (string query, params KeyValuePair<string, object>[] args) {
 			return CreateCommand (query, args).ExecuteReader ();
 		}
 
 		#region IDisposable implementation
 
+		/// <summary>
+		/// Releases all resource used by the <see cref="TheSilentNet.Database"/> object.
+		/// </summary>
+		/// <remarks>Call <see cref="Dispose"/> when you are finished using the <see cref="TheSilentNet.Database"/>. The
+		/// <see cref="Dispose"/> method leaves the <see cref="TheSilentNet.Database"/> in an unusable state. After calling
+		/// <see cref="Dispose"/>, you must release all references to the <see cref="TheSilentNet.Database"/> so the garbage
+		/// collector can reclaim the memory that the <see cref="TheSilentNet.Database"/> was occupying.</remarks>
 		public void Dispose () {
 			con.Close ();
 			con.Shutdown ();
--- src/TheSilentNet/TheSilentNet/DatabaseExtensions.cs
+++ src/TheSilentNet/TheSilentNet/DatabaseExtensions.cs
@@ -4,56 +4,131 @@ using System.Linq;
 
 namespace TheSilentNet
 {
+	/// <summary>
+	/// Database extensions.
+	/// </summary>
 	public static class DatabaseExtensions {
+
+		/// <summary>
+		/// An anonymous function that concatenates strings.
+		/// </summary>
+		/// <example>
+		/// Input:	"Hello", "World"
+		/// Output:	"Hello, World"
+		/// </example>
 		readonly static Func<string, string, string> aggrstr = (p1, p2) => string.Format ("{0}, {1}", p1, p2);
-		readonly static Dictionary<WhereOperation, string> oplookup = new Dictionary<WhereOperation, string> {
-			{ WhereOperation.Equals, "==" }
+
+		/// <summary>
+		/// A lookup table containing the string equivalent of a specific <see cref="WhereComparison"/>. 
+		/// </summary>
+		readonly static Dictionary<WhereComparison, string> oplookup = new Dictionary<WhereComparison, string> {
+			{ WhereComparison.Equals, "==" }
 		};
-		public enum WhereOperation {
+
+		/// <summary>
+		/// A comparison used in a WHERE statement.
+		/// </summary>
+		public enum WhereComparison {
 			Equals = 1 << 1,
 		}
-		public static string Select (this Database db, params string[] what) {
-			return what.Length == 0 ?
-				string.Format ("SELECT ()") : what.Length == 1 ?
-				string.Format ("SELECT {0}", what.First ()) :
-				string.Format ("SELECT ({0})", what.Aggregate (aggrstr));
+
+		/// <summary>
+		/// SELECT statement.
+		/// </summary>
+		/// <param name="db">Database.</param>
+		/// <param name="cols">Columns.</param>
+		public static string Select (this Database db, params string[] cols) {
+			return cols.Length == 0 ?
+				string.Format ("SELECT ()") : cols.Length == 1 ?
+				string.Format ("SELECT {0}", cols.First ()) :
+				string.Format ("SELECT ({0})", cols.Aggregate (aggrstr));
 		}
 
+		/// <summary>
+		/// FROM statement.
+		/// </summary>
+		/// <param name="query">Query.</param>
+		/// <param name="table">Table.</param>
 		public static string From (this string query, string table) {
 			return string.Format ("{0} FROM {1}", query, table);
 		}
 
+		/// <summary>
+		/// WHERE statement.
+		/// </summary>
+		/// <param name="query">Query.</param>
+		/// <param name="expr">Expression.</param>
 		public static string Where (this string query, string expr) {
 			return string.Format ("{0} WHERE {1}", query, expr);
 		}
 
-		public static string Where (this string query, string col, WhereOperation op, string comp) {
-			return string.Format ("{0} WHERE {1} {2} {3}", query, col, oplookup [op], comp);
+		/// <summary>
+		/// WHERE statement.
+		/// </summary>
+		/// <param name="query">Query.</param>
+		/// <param name="op1">First operand.</param>
+		/// <param name="comp">Type of comparison.</param>
+		/// <param name="op2">Second operand.</param>
+		public static string Where (this string query, string op1, WhereComparison comp, string op2) {
+			return string.Format ("{0} WHERE {1} {2} {3}", query, op1, oplookup [comp], op2);
 		}
 
+		/// <summary>
+		/// First part of an INSERT INTO statement.
+		/// </summary>
+		/// <param name="db">Database.</param>
+		/// <param name="table">Table.</param>
 		public static string Into (this Database db, string table) {
 			return string.Format ("INSERT INTO {0}", table);
 		}
 
+		/// <summary>
+		/// Second part of an INSERT INTO statement.
+		/// </summary>
+		/// <param name="query">Query.</param>
+		/// <param name="cols">Columns.</param>
 		public static string Insert (this string query, params string[] cols) {
 			return string.Format ("{0} ({1})", query, cols.Aggregate (aggrstr));
 		}
 
+		/// <summary>
+		/// VALUES statement.
+		/// </summary>
+		/// <param name="query">Query.</param>
+		/// <param name="cols">Columns.</param>
 		public static string Values (this string query, params object[] cols) {
 			return string.Format ("{0} ({1})", query, cols
 				.Select (c => c.ToString ()).Aggregate (aggrstr));
 		}
 
-		public static string Eq (this string query, object comp) {
-			return string.Format ("{0} == {1}", query, comp);
+		/// <summary>
+		/// Checks if the last operand in the query string equals the specified operand.
+		/// </summary>
+		/// <param name="query">Query.</param>
+		/// <param name="op">Operand.</param>
+		public static string Eq (this string query, object op) {
+			return string.Format ("{0} == {1}", query, op);
 		}
 
+		/// <summary>
+		/// LIMIT statement.
+		/// </summary>
+		/// <param name="query">Query.</param>
+		/// <param name="limit">Limit.</param>
+		/// <param name="limit2">Limit2.</param>
 		public static string Limit (this string query, int limit, int limit2 = -1) {
 			return limit2 == -1 ?
 				string.Format ("{0} LIMIT {1}", query, limit) :
 				string.Format ("{0} LIMIT {1},{2}", query, limit, limit2);
 		}
 
+		/// <summary>
+		/// Maps an object to a KeyValuePair suitable to be used
+		/// as a parameter for a prepared SQLite statement.
+		/// </summary>
+		/// <returns>The SQ lite parameter.</returns>
+		/// <param name="value">Value.</param>
+		/// <param name="key">Key.</param>
 		public static KeyValuePair<string, object> ToSQLiteParam (this object value, string key) {
 			return new KeyValuePair<string, object> (key, value);
 		}
