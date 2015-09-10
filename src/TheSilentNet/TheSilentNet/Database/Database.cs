8b3449b5442212ce316865caf5df965bf1d2b002
Splitty <splittydev@gmail.com>
2015-09-11 00:06:18 +0200

    Tried to fix tab/space issues

--- src/TheSilentNet/TheSilentNet/Database/Database.cs
+++ src/TheSilentNet/TheSilentNet/Database/Database.cs
@@ -10,31 +10,30 @@ namespace TheSilentNet {
     /// <summary>
     /// Database connector.
     /// </summary>
-    public class Database : Singleton<Database>, IDisposable
-	{
-		/// <summary>
-		/// The SQLite data source.
-		/// </summary>
-		const string DATA_SOURCE = "cipc.db";
+    public class Database : Singleton<Database>, IDisposable {
+        /// <summary>
+        /// The SQLite data source.
+        /// </summary>
+        const string DATA_SOURCE = "cipc.db";
 
-		/// <summary>
-		/// The SQLite connection.
-		/// </summary>
-		readonly SQLiteConnection con;
+        /// <summary>
+        /// The SQLite connection.
+        /// </summary>
+        readonly SQLiteConnection con;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="TheSilentNet.Database"/> class.
-		/// </summary>
-		public Database () {
-			
-			// Make sure that nobody directly instantiates this class.
-			// It must be instantiated from within the Instance () method.
-			Guard ();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TheSilentNet.Database"/> class.
+        /// </summary>
+        public Database () {
 
-			// Create and open the connection.
-			con = new SQLiteConnection {
-				ConnectionString = string.Format ("Data Source={0}", DATA_SOURCE)
-			}.OpenAndReturn ();
+            // Make sure that nobody directly instantiates this class.
+            // It must be instantiated from within the Instance () method.
+            Guard ();
+
+            // Create and open the connection.
+            con = new SQLiteConnection {
+                ConnectionString = string.Format ("Data Source={0}", DATA_SOURCE)
+            }.OpenAndReturn ();
 
             // Create the initial table layout.
             // This does nothing if the tables already exists.
@@ -42,26 +41,26 @@ namespace TheSilentNet {
                 .Pragma (FOREIGN_KEYS, ON)
                 .Append (TABLE_LAYOUT)
                 .Exec ();
-		}
+        }
 
-		/// <summary>
-		/// Adds a node to the 'cips' table.
-		/// </summary>
-		/// <returns><c>true</c>, if the node was added, <c>false</c> otherwise.</returns>
-		/// <param name="cip">cIP.</param>
-		/// <param name="type">Type.</param>
-		public bool AddNode (string cip, CipNodeType type) {
+        /// <summary>
+        /// Adds a node to the 'cips' table.
+        /// </summary>
+        /// <returns><c>true</c>, if the node was added, <c>false</c> otherwise.</returns>
+        /// <param name="cip">cIP.</param>
+        /// <param name="type">Type.</param>
+        public bool AddNode (string cip, CipNodeType type) {
             var pcip = cip.ToSQLiteParam ("@cip");
             var ptype = ((int)type).ToSQLiteParam ("@type");
             return QUERY_INSERT_NODE.ExecNonQuery (pcip, ptype) > 0;
-		}
+        }
 
-		/// <summary>
-		/// Adds a node to the 'cips' table.
-		/// </summary>
-		/// <returns><c>true</c>, if the node was added, <c>false</c> otherwise.</returns>
-		/// <param name="cip">cIP entry.</param>
-		public bool AddNode (CipEntry cip) => AddNode (cip.Value, cip.Type);
+        /// <summary>
+        /// Adds a node to the 'cips' table.
+        /// </summary>
+        /// <returns><c>true</c>, if the node was added, <c>false</c> otherwise.</returns>
+        /// <param name="cip">cIP entry.</param>
+        public bool AddNode (CipEntry cip) => AddNode (cip.Value, cip.Type);
 
         /// <summary>
 		/// Wraps a function call into a transaction.
@@ -100,21 +99,21 @@ namespace TheSilentNet {
             .Append (TABLE_LAYOUT)
             .ExecNonQuery () > 0;
 
-		/// <summary>
-		/// Gets a maximum of <paramref name="max"/> nodes from the 'cips' table.
-		/// </summary>
-		/// <returns>The nodes.</returns>
-		/// <param name="max">Node limit.</param>
-		/// <param name="tln">Whether only TLNs should be returned.</param>
-		public IEnumerable<CipEntry> GetNodes (int max = 1024, bool tln = false) {
-			var nodes = new List<CipEntry> ();
-			using (var reader = ExecReader ((tln ? QUERY_SELECT_TLN : QUERY_SELECT_CLN).Limit (max))) {
-				if (reader.HasRows)
-					while (reader.Read ())
-						nodes.Add (new CipEntry (reader.GetString (0), reader.GetByte (1)));
-			}
-			return nodes;
-		}
+        /// <summary>
+        /// Gets a maximum of <paramref name="max"/> nodes from the 'cips' table.
+        /// </summary>
+        /// <returns>The nodes.</returns>
+        /// <param name="max">Node limit.</param>
+        /// <param name="tln">Whether only TLNs should be returned.</param>
+        public IEnumerable<CipEntry> GetNodes (int max = 1024, bool tln = false) {
+            var nodes = new List<CipEntry> ();
+            using (var reader = ExecReader ((tln ? QUERY_SELECT_TLN : QUERY_SELECT_CLN).Limit (max))) {
+                if (reader.HasRows)
+                    while (reader.Read ())
+                        nodes.Add (new CipEntry (reader.GetString (0), reader.GetByte (1)));
+            }
+            return nodes;
+        }
 
         /// <summary>
         /// Gets a maximum of <paramref name="max"/> TLNs from the 'cips' table.
@@ -131,39 +130,39 @@ namespace TheSilentNet {
         /// <param name="args">Arguments.</param>
         [SuppressMessage ("Microsoft.Security", "CA2100")]
         public SQLiteCommand CreateCommand (string query, params KeyValuePair<string, object>[] args) {
-			var com = con.CreateCommand ();
-			com.CommandText = query;
-			foreach (var kvp in args)
-				com.Parameters.Add (new SQLiteParameter (kvp.Key, kvp.Value));
-			com.Prepare ();
-			return com;
-		}
+            var com = con.CreateCommand ();
+            com.CommandText = query;
+            foreach (var kvp in args)
+                com.Parameters.Add (new SQLiteParameter (kvp.Key, kvp.Value));
+            com.Prepare ();
+            return com;
+        }
 
-		/// <summary>
-		/// Executes a query.
-		/// </summary>
-		/// <returns>The number of rows inserted/updated affected by the query.</returns>
-		/// <param name="query">Query.</param>
-		/// <param name="args">Arguments.</param>
-		public int ExecNonQuery (string query, params KeyValuePair<string, object>[] args)
+        /// <summary>
+        /// Executes a query.
+        /// </summary>
+        /// <returns>The number of rows inserted/updated affected by the query.</returns>
+        /// <param name="query">Query.</param>
+        /// <param name="args">Arguments.</param>
+        public int ExecNonQuery (string query, params KeyValuePair<string, object>[] args)
             => CreateCommand (query, args).ExecuteNonQuery ();
 
-		/// <summary>
-		/// Executes a query.
-		/// </summary>
-		/// <returns>The first column of the resultset (if present), null otherwise.</returns>
-		/// <param name="query">Query.</param>
-		/// <param name="args">Arguments.</param>
-		public object ExecScalar (string query, params KeyValuePair<string, object>[] args)
+        /// <summary>
+        /// Executes a query.
+        /// </summary>
+        /// <returns>The first column of the resultset (if present), null otherwise.</returns>
+        /// <param name="query">Query.</param>
+        /// <param name="args">Arguments.</param>
+        public object ExecScalar (string query, params KeyValuePair<string, object>[] args)
             => CreateCommand (query, args).ExecuteScalar ();
 
-		/// <summary>
-		/// Executes a query.
-		/// </summary>
-		/// <returns>An SQLiteDataReader that can be used to access the resultset.</returns>
-		/// <param name="query">Query.</param>
-		/// <param name="args">Arguments.</param>
-		public SQLiteDataReader ExecReader (string query, params KeyValuePair<string, object>[] args)
+        /// <summary>
+        /// Executes a query.
+        /// </summary>
+        /// <returns>An SQLiteDataReader that can be used to access the resultset.</returns>
+        /// <param name="query">Query.</param>
+        /// <param name="args">Arguments.</param>
+        public SQLiteDataReader ExecReader (string query, params KeyValuePair<string, object>[] args)
             => CreateCommand (query, args).ExecuteReader ();
 
         #region IDisposable implementation
@@ -184,7 +183,7 @@ namespace TheSilentNet {
             }
         }
 
-		#endregion
-	}
+        #endregion
+    }
 }
 
